bf2c670d99365bad92949ffe4588b0cb3ca3bb34
DarlanNoetzold <darlan.noetzold@gmail.com>
2019-03-11 11:12:22 -0300

    Inicializando no GitHub

---
+++ Assets/Scripts/PipeCreator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeCreator : MonoBehaviour {
+
+	Player player;
+	float clock = 5f;
+
+	public GameObject PipeBase;
+	public float TimeToCreate = 3f;
+	public float Range = 2f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("player").GetComponent<Player> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.IsAlive == true && player.IsGameStarted == true) {
+			clock += Time.deltaTime;
+			if (clock >= TimeToCreate) {
+				clock = 0;
+
+				Vector3 pos = transform.position;
+				pos.y = pos.y - Random.Range (-Range, Range);
+				GameObject.Instantiate (PipeBase, pos, Quaternion.identity);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody2D rb;
+	AudioSource wingSound;
+	AudioSource hitSound;
+	AudioSource pointSound;
+	Vector3 initialPosition;
+
+	public float Force = 100f;
+	public bool IsAlive = true;
+	public bool IsGameStarted = false;
+	public bool IsGrounded = false;
+	public int Points = 0;
+	public Text PointText;
+	public Button BtnStart;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		wingSound = GetComponents<AudioSource> ()[0];
+		hitSound = GetComponents<AudioSource> ()[1];
+		pointSound = GetComponents<AudioSource> () [2];
+		initialPosition = transform.position;
+	}
+
+	void FixedUpdate () {
+		if (IsGameStarted == true) {
+			if (Input.GetMouseButtonDown (0) && IsAlive == true) {
+				rb.velocity = Vector2.zero;
+				rb.AddForce (new Vector2 (0, Force));
+				wingSound.Play ();
+			}
+
+			if (!IsGrounded) {
+				if (rb.velocity.y > 0) {
+					transform.rotation = Quaternion.Euler (0, 0, 45);
+				} else if (rb.velocity.y < 0) {
+					transform.rotation = Quaternion.Euler (0, 0, -45);
+				} else {
+					transform.rotation = Quaternion.Euler (0, 0, 0);
+				}
+			} else {
+				transform.rotation = Quaternion.Euler (0, 0, -90);
+			}
+		} else {
+			rb.velocity = Vector2.zero;
+			transform.position = initialPosition;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if ((coll.transform.CompareTag ("pipe") || coll.gameObject.name == "ground") && IsAlive == true) {
+			IsAlive = false;
+			IsGrounded = true;
+			hitSound.Play ();
+			BtnStart.gameObject.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.CompareTag ("pipe") && IsAlive == true) {
+			Points = Points + 1;
+			PointText.text = Points.ToString ();
+			pointSound.Play ();
+		}
+	}
+
+	public void OnGameStart ()
+	{
+		foreach (var item in GameObject.FindGameObjectsWithTag("pipe")) {
+			GameObject.Destroy (item);
+		}
+
+		Points = 0;
+		PointText.text = Points.ToString ();
+		transform.position = initialPosition;
+		IsAlive = true;
+		rb.velocity = Vector2.zero;
+		IsGameStarted = true;
+		BtnStart.gameObject.SetActive (false);
+		IsGrounded = false;
+	}
+}
